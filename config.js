d052004cd20aaabdd2ad6b1cea3214b50bc4d4dd
Jason Minghan Dong <jasonminghandong@gmail.com>
2023-06-29 11:54:29 -0400

    Added Java and Python files

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 console.log('cconfig.js');
-console.log('version12');
+console.log('versionn12');
---
+++ hello_world.java
@@ -0,0 +1,5 @@
+public class Main {
+    public statid void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
